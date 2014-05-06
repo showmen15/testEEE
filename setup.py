16ccdf7c7930f65719ba6e29f540626941ef7ee4
Paweł Suder <pawel@suder.info>
2014-05-06 13:46:33 +0200

    Update number version.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
                  'amber.tests': 'src/amber/tests',
                  'amber.tools': 'src/amber/tools'},
     install_requires=required,
-    version='1.0',
+    version='1.1',
     description='Amber drivers in python',
     author=u'Paweł Suder',
     author_email='pawel@suder.info',
