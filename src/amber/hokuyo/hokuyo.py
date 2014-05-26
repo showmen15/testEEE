49cdea1a4aad7076fc69543f639f5359812c91f1
Paweł Suder <pawel@suder.info>
2014-05-26 14:46:14 +0200

    Add restart for Hokuyo.

--- src/amber/hokuyo/hokuyo.py
+++ src/amber/hokuyo/hokuyo.py
@@ -1,9 +1,11 @@
-import serial
 import sys
 import os
+import traceback
+import time
+
+import serial
 
 from amber.hokuyo.hokuyo_common import HokuyoController
-
 from amber.tools import serial_port, config
 
 
@@ -20,5 +22,12 @@ if __name__ == '__main__':
     serial = serial.Serial(port=SERIAL_PORT, baudrate=BAUD_RATE, timeout=TIMEOUT)
     port = serial_port.SerialPort(serial)
 
-    controller = HokuyoController(sys.stdin, sys.stdout, port)
-    controller()
+    while True:
+        # noinspection PyBroadException
+        try:
+            controller = HokuyoController(sys.stdin, sys.stdout, port)
+            controller()
+        except BaseException as e:
+            print 'error: %s' % str(e)
+            traceback.print_exc()
+            time.sleep(5)
