6b5cd40b19734178e2c29739f34caddede5c9f91
Paweł Suder <pawel@suder.info>
2014-05-29 10:29:43 +0200

    Update desc for Dummy driver implementation.

--- src/amber/dummy/dummy.py
+++ src/amber/dummy/dummy.py
@@ -18,12 +18,21 @@ logging.config.fileConfig('%s/dummy.ini' % pwd)
 
 
 class Dummy(object):
+    """
+    Example implementation.
+    """
+
     def __init__(self):
         self.message = 'Message'
         self.enable = False
 
 
 class DummyController(MessageHandler):
+    """
+    Example implementation of driver.
+    Need to extends `MessageHandler` from `amber.common.amber_pipes`.
+    """
+
     def __init__(self, pipe_in, pipe_out):
         super(DummyController, self).__init__(pipe_in, pipe_out)
         self.__dummy = Dummy()
@@ -34,6 +43,15 @@ class DummyController(MessageHandler):
         self.__logger = logging.getLogger(LOGGER_NAME)
 
     def handle_data_message(self, header, message):
+        """
+        Handling DATA message type. Must be implemented.
+        Use extensions and checking if extension exists in message,
+        to determine what to do with message.
+
+        :param header:
+        :param message:
+        :return:
+        """
         if message.HasExtension(dummy_pb2.enable):
             self.__handle_set_enable(header, message)
 
@@ -47,17 +65,40 @@ class DummyController(MessageHandler):
             self.__logger.warning('No request in message')
 
     def __handle_set_enable(self, header, message):
+        """
+        Example operation, setting enable flag.
+
+        :param header:
+        :param message:
+        :return:
+        """
         value = message.Extensions[dummy_pb2.enable]
         self.__logger.debug('Set enable to %s' % value)
         self.__dummy.enable = value
 
     def __handle_set_message(self, header, message):
+        """
+        Example operation, setting message.
+
+        :param header:
+        :param message:
+        :return:
+        """
         value = message.Extensions[dummy_pb2.message]
         self.__logger.debug('Set message to %s' % value)
         self.__dummy.message = value
 
     @MessageHandler.handle_and_response
     def __handle_get_status(self, received_header, received_message, response_header, response_message):
+        """
+        Example operation, receive values.
+
+        :param received_header:
+        :param received_message:
+        :param response_header:
+        :param response_message:
+        :return:
+        """
         self.__logger.debug('Get status')
 
         response_message.Extensions[dummy_pb2.enable] = self.__dummy.enable
@@ -66,6 +107,13 @@ class DummyController(MessageHandler):
         return response_header, response_message
 
     def handle_subscribe_message(self, header, message):
+        """
+        Handle SUBSCRIBE message type. Must be implemented.
+
+        :param header:
+        :param message:
+        :return:
+        """
         self.__logger.debug('Subscribe action')
 
         no_subscribers = (len(self.__subscribers) == 0)
@@ -76,10 +124,23 @@ class DummyController(MessageHandler):
             self.__subscribe_thread.start()
 
     def handle_unsubscribe_message(self, header, message):
+        """
+        Handle UNSUBSCRIBE message type. Must be implemented.
+
+        :param header:
+        :param message:
+        :return:
+        """
         self.__logger.debug('Unsubscribe action')
         map(lambda client_id: self.__remove_subscriber(client_id), header.clientIDs)
 
     def handle_client_died_message(self, client_id):
+        """
+        Handle CLIENT_DIES message type. Must be implemented.
+
+        :param client_id:
+        :return:
+        """
         self.__logger.info('Client %d died' % client_id)
         self.__remove_subscriber(client_id)
 
@@ -90,6 +151,11 @@ class DummyController(MessageHandler):
             self.__logger.warning('Client %d does not registered as subscriber' % client_id)
 
     def __run(self):
+        """
+        Implementation of function used to service subscribed clients.
+
+        :return:
+        """
         while len(self.__subscribers) > 0:
             response_header = drivermsg_pb2.DriverHdr()
             response_message = drivermsg_pb2.DriverMsg()
@@ -107,5 +173,7 @@ class DummyController(MessageHandler):
 
 
 if __name__ == '__main__':
+    # Create controller and run it.
     controller = DummyController(sys.stdin, sys.stdout)
+    # It's running in infinite loop.
     controller()
