ac9200ec0140909c4b88a8e3c81fed60973468b5
Paweł Suder <pawel@suder.info>
2014-07-01 07:15:41 +0200

    Add profiler lines.

--- bin/dummy.sh
+++ bin/dummy.sh
@@ -7,5 +7,19 @@ if [ -d ${__dir}/__envi ]
 then
     . ${__dir}/__envi/bin/activate
     export PYTHONPATH=${__dir}/src
-    ${__dir}/__envi/bin/python -u ${PYTHONPATH}/amber/dummy/dummy.py
+
+    if [ -z "${_APP_PROFILE}" ];
+    then
+        ${__dir}/__envi/bin/python -u ${PYTHONPATH}/amber/dummy/dummy.py
+    else
+        export _APP_TEMP=$(mktemp -d)
+
+        ${__dir}/__envi/bin/python -u -m cProfile -o ${_APP_TEMP}/output.pstats ${PYTHONPATH}/amber/dummy/dummy.py
+
+        ${__dir}/__envi/bin/gprof2dot -f pstats --output ${_APP_TEMP}/output.dot ${_APP_TEMP}/output.pstats
+        # TODO(paoolo) check if `dot` exists?
+        /usr/bin/dot -Tpng ${_APP_TEMP}/output.dot -o ${__dir}/profile-dummy-$(date +"%Y%m%d-%H%M%S").png
+
+        rm -rf ${_APP_TEMP}
+    fi
 fi
