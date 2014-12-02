9590f8b7cdfc078e83f3ed99c7661df36959e6f3
Paweł Suder <pawel@suder.info>
2014-12-02 16:07:47 +0100

    Update proto driver message, add proto message for drive to point driver

--- protoc.sh
+++ protoc.sh
@@ -7,6 +7,6 @@ export AMBER_DIR=${ROOT_DIR}/src/amber
 export COMMON_DIR=${AMBER_DIR}/common
 
 protoc -I ${COMMON_DIR} --python_out=${COMMON_DIR} ${COMMON_DIR}/drivermsg.proto
-for pp in hokuyo dummy; do
+for pp in hokuyo dummy drive_to_point; do
     protoc -I ${COMMON_DIR} -I ${AMBER_DIR}/${pp} --python_out=${AMBER_DIR}/${pp} ${AMBER_DIR}/${pp}/${pp}.proto
 done
