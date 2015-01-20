1894166650e09b7914d9b432772c768cb4c3877c
Paweł Suder <pawel@suder.info>
2015-01-20 19:41:58 +0100

    Wait for the first location.

--- src/amberdriver/drive_to_point/drive_to_point.py
+++ src/amberdriver/drive_to_point/drive_to_point.py
@@ -75,7 +75,10 @@ class DriveToPoint(object):
 
     def location_loop(self):
         sleep_interval = 0.5
-        last_location = self.__location_proxy.get_location().get_location()
+        last_location = self.__location_proxy.get_location()
+        last_location.wait_available(0)
+        last_location = last_location.get_location()
+
         time.sleep(sleep_interval)
         while self.is_active():
             current_location = self.__location_proxy.get_location().get_location()
