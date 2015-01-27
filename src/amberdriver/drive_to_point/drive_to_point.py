2b52b0876bf1aa259b2a7ef8ec074049d2246c4b
Paweł Suder <pawel@suder.info>
2015-01-27 20:24:48 +0100

    Drive to point (Controller) lock improvements.

--- src/amberdriver/drive_to_point/drive_to_point.py
+++ src/amberdriver/drive_to_point/drive_to_point.py
@@ -22,6 +22,19 @@ def bound_sleep_interval(value, min_value=0.2, max_value=2.0):
     return value if min_value < value < max_value else max_value if value > max_value else min_value
 
 
+def compute_sleep_interval(current_timestamp, last_timestamp, sleep_interval,
+                           max_interval=2.0, alpha=0.5):
+    """
+    compute_sleep_interval(current timestamp in ms, last timestamp in ms, actual sleep interval) -> new sleep interval
+    """
+    timestamp_interval = current_timestamp - last_timestamp
+    timestamp_interval /= 1000.0
+    if timestamp_interval < max_interval:
+        sleep_interval += alpha * (timestamp_interval - sleep_interval)
+        sleep_interval = bound_sleep_interval(sleep_interval)
+    return sleep_interval
+
+
 class DriveToPoint(object):
     MAX_SPEED = 300
     DRIVING_ALPHA = 3.0  # cut at 60st
@@ -33,148 +46,116 @@ class DriveToPoint(object):
 
         self.__next_targets, self.__visited_targets, self.__current_location = [], [], None
         self.__next_targets_timestamp = 0.0
-        self.__targets_and_location_lock = threading.Condition()
+        self.__targets_lock = threading.Condition()
 
         self.__is_active = True
-        self.__is_active_lock = threading.Condition()
-
         self.__drive_allowed = False
 
         self.__old_left, self.__old_right = 0.0, 0.0
         self.__logger = logging.getLogger(LOGGER_NAME)
 
     def set_targets(self, targets):
+        self.__targets_lock.acquire()
         try:
-            self.__targets_and_location_lock.acquire()
             self.__next_targets = targets
             self.__next_targets_timestamp = time.time()
             self.__drive_allowed = len(targets) > 0
             self.__visited_targets = []
+            self.__targets_lock.notify_all()
         finally:
-            self.__targets_and_location_lock.release()
+            self.__targets_lock.release()
 
     def get_next_targets_and_location(self):
+        self.__targets_lock.acquire()
         try:
-            self.__targets_and_location_lock.acquire()
             return self.__next_targets[:], self.__current_location
         finally:
-            self.__targets_and_location_lock.release()
+            self.__targets_lock.release()
 
     def get_next_target_and_location(self):
+        self.__targets_lock.acquire()
         try:
-            self.__targets_and_location_lock.acquire()
             _next_target = self.__next_targets[0] if len(self.__next_targets) > 0 else (0, 0, 0)
             return _next_target, self.__current_location
         finally:
-            self.__targets_and_location_lock.release()
+            self.__targets_lock.release()
 
     def get_visited_targets_and_location(self):
+        self.__targets_lock.acquire()
         try:
-            self.__targets_and_location_lock.acquire()
             return self.__visited_targets[:], self.__current_location
         finally:
-            self.__targets_and_location_lock.release()
+            self.__targets_lock.release()
 
     def get_visited_target_and_location(self):
+        self.__targets_lock.acquire()
         try:
-            self.__targets_and_location_lock.acquire()
             _visited_target = self.__visited_targets[-1] if len(self.__visited_targets) > 0 else (0, 0, 0)
             return _visited_target, self.__current_location
         finally:
-            self.__targets_and_location_lock.release()
+            self.__targets_lock.release()
 
     def location_loop(self):
         sleep_interval = 0.5
-
         last_location = self.__location_proxy.get_location()
         last_location = last_location.get_location()
-
         time.sleep(sleep_interval)
-        while self.is_active():
+        while self.__is_active:
             current_location = self.__location_proxy.get_location()
             current_location = current_location.get_location()
-            self.__set_current_location(current_location)
-
+            self.__current_location = current_location
             try:
-                location_interval = current_location[DriveToPoint.TIMESTAMP_FIELD] - \
-                                    last_location[DriveToPoint.TIMESTAMP_FIELD]
-                location_interval /= 1000.0
-                if location_interval < 2.0:
-                    sleep_interval += 0.5 * (location_interval - sleep_interval)
-                    sleep_interval = bound_sleep_interval(sleep_interval)
+                sleep_interval = compute_sleep_interval(current_location[DriveToPoint.TIMESTAMP_FIELD],
+                                                        last_location[DriveToPoint.TIMESTAMP_FIELD],
+                                                        sleep_interval)
             except TypeError:
+                # FIXME: silent pass?
                 pass
-
             last_location = current_location
-
             time.sleep(sleep_interval)
 
     def driving_loop(self):
-        while self.is_active():
+        while self.__is_active:
+            self.__wait_for_new_targets()
             try:
-                while self.is_active():
-                    target = self.__get_next_target()
+                while self.__is_active:
+                    target = self.__next_targets[0]
                     self.__drive_to(target, self.__next_targets_timestamp)
                     self.__add_target_to_visited(target)
             except IndexError:
+                self.__logger.warning('Next targets list is empty, stop driving.')
                 self.__stop()
-            time.sleep(0.1)
 
-    def is_active(self):
+    def __wait_for_new_targets(self):
+        self.__targets_lock.acquire()
         try:
-            self.__is_active_lock.acquire()
-            return self.__is_active
+            while not len(self.__next_targets) > 0:
+                self.__targets_lock.wait()
         finally:
-            self.__is_active_lock.release()
-
-    def __get_next_target(self):
-        try:
-            self.__targets_and_location_lock.acquire()
-            return self.__next_targets[0]
-        finally:
-            self.__targets_and_location_lock.release()
-
-    def __set_current_location(self, location):
-        try:
-            self.__targets_and_location_lock.acquire()
-            self.__current_location = location
-        finally:
-            self.__targets_and_location_lock.release()
-
-    def __get_current_location(self):
-        try:
-            self.__targets_and_location_lock.acquire()
-            return self.__current_location
-        finally:
-            self.__targets_and_location_lock.release()
+            self.__targets_lock.release()
 
     def __add_target_to_visited(self, target):
+        self.__targets_lock.acquire()
         try:
-            self.__targets_and_location_lock.acquire()
-            try:
-                self.__next_targets.remove(target)
-                self.__visited_targets.append(target)
-            except ValueError:
-                self.__logger.warning('target %s not in targets list, dropping..', str(target))
+            self.__next_targets.remove(target)
+            self.__visited_targets.append(target)
+        except ValueError:
+            self.__logger.warning('Target %s is not in next targets list, not added to visited targets list.',
+                                  str(target))
         finally:
-            self.__targets_and_location_lock.release()
+            self.__targets_lock.release()
 
     def terminate(self):
-        try:
-            self.__is_active_lock.acquire()
-            self.__is_active = False
-        finally:
-            self.__is_active_lock.release()
+        self.__is_active = False
 
     def __drive_to(self, target, next_targets_timestamp):
         self.__logger.info('Drive to %s', str(target))
 
         sleep_interval = 0.5
-
-        location = self.__get_current_location()
+        location = self.__current_location
         while location is None:
             time.sleep(sleep_interval)
-            location = self.__get_current_location()
+            location = self.__current_location
 
         while not DriveToPoint.target_reached(location, target) and self.__drive_allowed \
                 and not self.__next_targets_timestamp > next_targets_timestamp:
@@ -184,19 +165,11 @@ class DriveToPoint(object):
             self.__roboclaw_proxy.send_motors_command(left, right, left, right)
 
             time.sleep(sleep_interval)
-
             old_location = location
-            location = self.__get_current_location()
-
-            try:
-                location_interval = location[DriveToPoint.TIMESTAMP_FIELD] - \
-                                    old_location[DriveToPoint.TIMESTAMP_FIELD]
-                location_interval /= 1000.0
-                if location_interval < 2.0:
-                    sleep_interval += 0.5 * (location_interval - sleep_interval)
-                    sleep_interval = bound_sleep_interval(sleep_interval)
-            except TypeError:
-                pass
+            location = self.__current_location
+            sleep_interval = compute_sleep_interval(location[DriveToPoint.TIMESTAMP_FIELD],
+                                                    old_location[DriveToPoint.TIMESTAMP_FIELD],
+                                                    sleep_interval)
 
         self.__logger.info('Target %s reached', str(target))
 
@@ -212,14 +185,12 @@ class DriveToPoint(object):
     def target_reached(location, target):
         target_x, target_y, target_radius = target
         location_x, location_y, _, _, _ = location
-
         try:
             diff_x = location_x - target_x
             diff_y = location_y - target_y
-
             return math.pow(diff_x, 2) + math.pow(diff_y, 2) < math.pow(target_radius, 2)
-
         except TypeError:
+            # FIXME: silent pass?
             return False
 
     @staticmethod
