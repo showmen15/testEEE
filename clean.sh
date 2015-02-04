3974c24f924f55897ce0c06e4582f31c55a7109c
Paweł Suder <pawel@suder.info>
2015-02-04 15:24:23 +0100

    Update test execution environment

--- clean.sh
+++ clean.sh
@@ -3,6 +3,6 @@
 export __dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 set -x
-rm -rf ${__dir}/*.egg-info/ ${__dir}/build/ ${__dir}/dist/
+rm -rf ${__dir}/*.egg-info/ ${__dir}/build/ ${__dir}/dist/ ${__dir}/*.egg
 find ${__dir} -name *.pyc -exec rm {} \;
 find ${__dir} -name *_pb2.py -exec rm {} \;
