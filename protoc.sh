d0ab6da6254febb49bd76b18fc4445ddd10f61ca
Paweł Suder <pawel@suder.info>
2015-02-09 19:33:52 +0100

    Recovery Roboclaw driver to automatic drive support mechanism.

--- protoc.sh
+++ protoc.sh
@@ -7,7 +7,7 @@ export AMBER_DIR=${ROOT_DIR}/src/amberdriver
 export COMMON_DIR=${AMBER_DIR}/common
 
 protoc -I ${COMMON_DIR} --python_out=${COMMON_DIR} ${COMMON_DIR}/drivermsg.proto
-for pp in collision_avoidance dummy drive_to_point hokuyo; do
+for pp in collision_avoidance dummy drive_to_point hokuyo roboclaw; do
     protoc -I ${COMMON_DIR} -I ${AMBER_DIR}/${pp} --python_out=${AMBER_DIR}/${pp} ${AMBER_DIR}/${pp}/${pp}.proto
 done
 protoc -I ${COMMON_DIR} -I ${AMBER_DIR}/collision_avoidance --python_out=${AMBER_DIR}/collision_avoidance ${AMBER_DIR}/collision_avoidance/roboclaw.proto
