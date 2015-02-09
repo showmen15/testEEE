0b54aa08bc14e26821343af491a5d000b380ff97
Paweł Suder <pawel@suder.info>
2015-02-09 19:14:43 +0100

    Add some functions

--- src/amberdriver/collision_avoidance/collision_avoidance_logic.py
+++ src/amberdriver/collision_avoidance/collision_avoidance_logic.py
@@ -18,16 +18,16 @@ def get_soft_limit(current_speed, max_speed, soft_limit, hard_limit, alpha):
     return alpha * soft_limit * (current_speed / max_speed) + hard_limit + 50.0
 
 
-def points_radians(points):
+def convert_angles_to_radians(points):
     return map(lambda (angle, distance): (math.radians(angle), distance), points)
 
 
-def points_degrees(points):
+def convert_angles_to_degrees(points):
     return map(lambda (angle, distance): (math.degrees(angle), distance), points)
 
 
 def get_min_distance(scan, current_angle, scanner_dist_offset, angle_range):
-    points = points_radians(scan)
+    points = convert_angles_to_radians(scan)
     min_distance = None
     min_distance_angle = None
 
@@ -42,7 +42,7 @@ def get_min_distance(scan, current_angle, scanner_dist_offset, angle_range):
 
 
 def get_max_distance(scan, current_angle, scanner_dist_offset, angle_range):
-    points = points_radians(scan)
+    points = convert_angles_to_radians(scan)
     max_distance = None
     max_distance_angle = None
     max_diff_angle = 0.0
@@ -57,3 +57,85 @@ def get_max_distance(scan, current_angle, scanner_dist_offset, angle_range):
                 max_diff_angle = diff_angle
 
     return max_distance, max_distance_angle
+
+
+def analyze_scan(scan):
+    points = convert_angles_to_radians(scan)
+    for angle, distance in points:
+        x, y = distance * math.cos(angle), distance * math.sin(angle)
+
+
+def simple_importance_angle_level(angle):
+    if math.fabs(angle) < math.pi / 2.0:
+        return 1.0 - 2.0 * math.fabs(angle) / math.pi
+    return 0.0
+
+
+def simple_distance_level(distance, max_distance=5000.0):
+    """ max_distance in [mm] """
+    return distance / max_distance
+
+
+def average(values):
+    return reduce(lambda x, y: x + y, values) / len(values)
+
+
+def distance_danger_level(scan,
+                          importance_angle_level_func=simple_importance_angle_level,
+                          distance_level_func=simple_distance_level,
+                          compute_level_func=average):
+    points = convert_angles_to_radians(scan)
+    levels = []
+    for angle, distance in points:
+        importance_angle_level = importance_angle_level_func(angle)
+        distance_level = distance_level_func(distance)
+        levels.append(importance_angle_level * distance_level)
+    return compute_level_func(levels)
+
+
+def find_continuities(scan, max_continuity_interval=10):
+    """ max_continuity_interval in [mm] for distance 100 [mm] """
+    points = convert_angles_to_radians(scan)
+    continuities = []
+    old_x, old_y = None, None
+    first_x, first_y = None, None
+    for angle, distance in sorted(points.items()):
+        x, y = distance * math.cos(angle), distance * math.sin(angle)
+        if old_x is None and old_y is None:
+            old_x, old_y = x, y
+            first_x, first_y = x, y
+        else:
+            limit = math.pow(distance * max_continuity_interval * 0.01, 2)
+            if math.pow(old_x - x, 2) > limit or math.pow(old_y - y, 2) > limit:
+                first = (first_x, first_y)
+                last = (old_x, old_y)
+                continuities.append((first, last))
+                first_x, first_y = x, y
+            else:
+                old_x, old_y = x, y
+    return continuities
+
+
+def find_locals_min_max(scan, interval=50, min_distance=100.0, max_distance=5000.0):
+    """ interval in 50 [mm] """
+    points = convert_angles_to_radians(scan)
+    local_mini, local_maxi = None, None
+    local_minis, local_maxis = [], []
+    prev_distance = None
+    for angle, distance in sorted(points.items()):
+        if min_distance < distance < max_distance:
+            if prev_distance is None:
+                prev_distance = distance
+            else:
+                if distance - prev_distance > interval:
+                    local_maxi = (angle, distance)
+                    if local_mini is not None:
+                        local_minis.append(local_mini)
+                        local_mini = None
+                elif distance - prev_distance < -interval:
+                    local_mini = (angle, distance)
+                    if local_maxi is not None:
+                        local_maxis.append(local_maxi)
+                        local_maxi = None
+                prev_distance = distance
+    return local_minis, local_maxis
