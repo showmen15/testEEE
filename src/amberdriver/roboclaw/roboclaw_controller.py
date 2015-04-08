78e0de447f00a720c12bbad1873f49a8e8dbaee0
Paweł Suder <pawel@suder.info>
2015-04-08 06:25:47 +0200

    Fix GPIO usage.

--- src/amberdriver/roboclaw/roboclaw_controller.py
+++ src/amberdriver/roboclaw/roboclaw_controller.py
@@ -8,7 +8,6 @@ import time
 
 import serial
 import os
-
 from ambercommon.common import runtime
 
 from amberdriver.common.message_handler import MessageHandler
@@ -128,9 +127,9 @@ class RoboclawDriver(object):
         self.__motors_stop_timer_enabled, self.__battery_low = False, False
         self.__reset_time, self.__motors_stop_time = 0.0, 0.0
 
-        self.__reset_gpio = open(RESET_GPIO_PATH, mode='w')
-        self.__led1_gpio = open(LED1_GPIO_PATH, mode='w')
-        self.__led2_gpio = open(LED2_GPIO_PATH, mode='w')
+        self.__reset_gpio = open(RESET_GPIO_PATH, mode='ab')
+        self.__led1_gpio = open(LED1_GPIO_PATH, mode='ab')
+        self.__led2_gpio = open(LED2_GPIO_PATH, mode='ab')
 
         self.__overheated = False
         self.__roboclaw_disabled = False
@@ -141,10 +140,10 @@ class RoboclawDriver(object):
 
         runtime.add_shutdown_hook(self.terminate)
 
-        if self.__led1_gpio.write('0') < 0:
-            self.__logger.warn('led1 gpio set to up failed')
-        if self.__led2_gpio.write('1') < 0:
-            self.__logger.warn('led2 gpio set to down failed')
+        self.__led1_gpio.write('0')
+        self.__led1_gpio.flush()
+        self.__led2_gpio.write('1')
+        self.__led2_gpio.flush()
 
     def set_controller(self, _):
         pass
@@ -179,8 +178,8 @@ class RoboclawDriver(object):
         if self.__roboclaw_disabled:
             return
 
-        if self.__led1_gpio.write('0') < 0:
-            self.__logger.warn('led1 gpio set to up failed')
+        self.__led1_gpio.write('0')
+        self.__led1_gpio.flush()
 
         front_left = to_qpps(front_left)
         front_right = to_qpps(front_right)
@@ -198,8 +197,8 @@ class RoboclawDriver(object):
         finally:
             self.__roboclaw_lock.release()
 
-        if self.__led1_gpio.write('1') < 0:
-            self.__logger.warn('led1 gpio set to down failed')
+        self.__led1_gpio.write('1')
+        self.__led1_gpio.flush()
 
     def stop(self):
         self.__roboclaw_lock.acquire()
@@ -306,8 +305,8 @@ class RoboclawDriver(object):
                     if front_error_status in [0x01, 0x02] or rear_error_status in [0x01, 0x02]:
                         self.__reset_and_wait()
                     elif front_error_status == 0x20 or rear_error_status == 0x20:
-                        if self.__led2_gpio.write('0') < 0:
-                            self.__logger.warn('led2 gpio set to up failed')
+                        self.__led2_gpio.write('0')
+                        self.__led2_gpio.flush()
                         self.__battery_low = True
                         return
 
